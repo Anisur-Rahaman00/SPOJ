a659e9ef530613f0cb05db94f595a6b12f295a90
Anisur Rahaman <60539253+anis017@users.noreply.github.com>
2022-03-07 00:21:31 +0600

    Create SumOfDigits.cpp

---
+++ SumOfDigits.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+
+#define I_O ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+//~~~~~~~~~~~~ Sort Form Of Long~~~~~~~~~~~//
+#define ll long long int
+#define lls int64_t
+#define ld long double
+#define db double
+#define ull unsigned long long int
+
+//~~~~~~~~~~~~~~Pair~~~~~~~~~~~~~~~~~~//
+#define pii pair<int, int>
+#define pll pair<lls, lls>
+#define pdd pair<db,db>
+#define psi pair<string,int>
+#define vi vector<int>
+#define vl vector<lls>
+#define vd vector<db>
+#define vs vector<string>
+#define vb vector<bool>
+#define vpi vector<pii>
+#define vpl vector<pll>
+#define vpd vector<pdd>
+#define vpsi vector<psi>
+#define vvi vector<vector<int>>
+
+//~~~~~~~~~~~~~~Shorts~~~~~~~~~~~~~~~~~//
+#define ff first
+#define ss second
+#define Br break
+#define Cn continue
+#define al(v) v.begin(),v.end()
+#define alr(v) v.rbegin(), v.rend()
+#define srt(v) sort(al(v))
+#define srtr(v) sort(al(v), greater<int>());
+#define len(x) ((int)(x).size())
+#define rev(v) reverse(al(v))
+#define btcnt(n) __builtin_popcount(n)
+#define acl(v, n) accumulate(al(v), n)
+#define Lrt(s, c) rotate(s.begin(), s.begin() + c, s.end())
+#define Rrt(s, c) rotate(s.begin(), s.begin() + s.size() - c, s.end())
+#define lb(v, kk) lower_bound(al(v), kk) - v.begin()
+#define ub(v, kk) upper_bound(al(v), kk) - v.begin()
+#define tpu(str) transform(al(str), str.begin(), ::toupper)
+#define tpl(str) transform(al(str), str.begin(), ::tolower)
+#define cignr cin.ignore(numeric_limits<streamsize>::max(), '\n');
+#define mxv(v) *max_element(al(v))
+#define mnv(v) *min_element(al(v))
+
+const int MOD = 1e9 + 7;
+const lls INF = 2e18;
+const int mxn = 2e9 + 9;
+const int mxd = 2e5 + 5;
+const int mxa = 2e6 + 5;
+
+//~~~~~~~~~~~~~~~~~~Function~~~~~~~~~~~~~~~~~~~~//
+lls gcd(lls a, lls b){ if(b == 0LL) return a; return gcd(b, a % b); }
+lls lcm(lls a, lls b){ return (a / gcd(a, b) * b); }
+lls maxll(lls x, lls y){ return x > y ? x : y; }
+lls minll(lls x, lls y){ return x < y ? x : y; }
+
+//~~~~~~~~~~~~~~~Loops and Short~~~~~~~~~~~~~~~~//
+
+#define PI acos(-1)
+#define N '\n'
+#define fopen freopen("input.txt", "r", stdin);
+#define rep(i, n) for(lls i = 0; (lls)i < n; i++)
+#define test_case() int T; T = 1; while(T--)
+
+using namespace std;
+
+// ===================================~~~~~~ SOLUTION STARTS HERE ~~~~~~=================================== //
+
+//int a, b, c, d, e, res = 0, ans = 0, prod = 1, n, m, cnt = 0, k, l, r, s, t, x, y, f, i, j, p;
+
+
+int arr[1001];
+lls dp[11][100][2];
+
+lls getSum(int pos, int n, lls sum, int f) {
+   if (pos > n) return sum;
+
+   if (dp[pos][sum][f] != -1) return dp[pos][sum][f];
+   int limit = 9;
+   if (!f) limit = arr[pos];
+
+   lls res = 0;
+   for (int i = 0; i <= limit; i++) {
+      if (f or i < limit) res += getSum(pos + 1, n, sum + i, 1);
+      else res += getSum(pos + 1, n, sum + i, 0);
+   }
+
+   return dp[pos][sum][f] = res;
+}
+
+int getSum(lls n) {
+   int sum = 0;
+   while (n) {
+      sum += n % 10;
+      n /= 10;
+   }
+
+   return sum;
+}
+
+void Run_Case() 
+{
+   lls a, b; 
+   while (cin >> a >> b and a != -1 and b != -1) {
+      string s = to_string(a), t = to_string(b);
+
+      memset(dp, -1, sizeof(dp));
+      
+      for (int i = 0; i < s.size(); i++) arr[i + 1] = s[i] - '0';
+
+      lls left = getSum(1, s.size(), 0, 0);
+
+      for (int i = 0; i < t.size(); i++) arr[i + 1] = t[i] - '0';
+
+      memset(dp, -1, sizeof(dp));
+      lls right = getSum(1, t.size(), 0, 0);
+
+      cout << right - left + getSum(a) << N;
+   }
+}
+
+int main()
+{
+   I_O
+   test_case()
+   {
+      Run_Case();
+   }
+
+   return 0;
+}
